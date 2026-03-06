e4e381b41f36c7757af82e5053be7de8224782e2
L1dzirable <ton_email_github@example.com>
2026-03-06 15:09:20 +0100

    remove react query wrapper

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,4 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-<AppRoutes />
-import { QueryClientProvider } from "@tanstack/react-query";
 import NotFound from "./pages/not-found";
 import Home from "./pages/Home";
 
@@ -15,11 +12,8 @@ function Router() {
 }
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-        <Router />
-    </QueryClientProvider>
-  );
+  return <Router />;
+}
 }
 
 export default App;
