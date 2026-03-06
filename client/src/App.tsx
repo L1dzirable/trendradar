d9ebfad1b90957780e2e6bca5ceecfd0d93c2668
L1dzirable <ton_email_github@example.com>
2026-03-06 14:53:37 +0100

    remove tooltip import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
+<AppRoutes />
 import { QueryClientProvider } from "@tanstack/react-query";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 
@@ -17,9 +17,7 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
         <Router />
-      </TooltipProvider>
     </QueryClientProvider>
   );
 }
