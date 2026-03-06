6005b346edf6b00cb75d31cc00ddd3b851002e87
L1dzirable <ton_email_github@example.com>
2026-03-06 16:48:41 +0100

    simplify app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,6 @@
 
-import { Switch, Route } from "wouter";
-import NotFound from "./pages/not-found";
 import Home from "./pages/Home";
 
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
+export default function App() {
+  return <Home />;
 }
-
-function App() {
-  return <Router />;
-}
-
-export default App;
